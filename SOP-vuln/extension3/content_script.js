71eb23a452ffcb712366527ba26840ef942d8f0a
Henke <kultala@kth.se>
2021-06-04 11:01:16 +0200

    Some cleanup

--- SOP-vuln/extension3/content_script.js
+++ SOP-vuln/extension3/content_script.js
@@ -1,9 +1,3 @@
-/**
-* Content-script that runs in URLs using http or https.
-* (See https://stackoverflow.com/a/16096545 for regex in manifest.json)
-*
-* run_at is "document_idle", reference: https://developer.chrome.com/docs/extensions/mv3/content_scripts/#document_idle, possibly found here: https://stackoverflow.com/questions/6497548/chrome-extension-make-it-run-every-page-load
-*/
 
 DEBUG = true;
 let extensionID = "FILL_IN"
--- SOP-vuln/webserver3/app.js
+++ SOP-vuln/webserver3/app.js
@@ -1,24 +1,7 @@
 
 const express = require('express')
-const session = require('express-session')
-const flash = require('connect-flash')
-
-var path = require('path');
-var fs = require('fs');
 
 var app = express()
-var dir = path.join(__dirname, 'public/images');
-
-var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
-};
 
 // App setup
 app
@@ -26,23 +9,6 @@ app
   .get('/', (req, res) => {
     res.render('home', {})
   })
-  .get('*', function (req, res) {
-    console.log("New request caught by the catch all")
-    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
-    if (file.indexOf(dir + path.sep) !== 0) {
-        return res.status(403).end('Forbidden');
-    }
-    var type = mime[path.extname(file).slice(1)] || 'text/plain';
-    var s = fs.createReadStream(file);
-    s.on('open', function () {
-        res.set('Content-Type', type);
-        s.pipe(res);
-    });
-    s.on('error', function () {
-        res.set('Content-Type', 'text/plain');
-        res.status(404).end('Not found');
-    });
-})
 
   .listen('3000', () => {
     console.log('Server now listening on port 3000...')
